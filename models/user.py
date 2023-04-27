67395cebd601c62d610487f2b03eccb96567844d
Goldjay94 <genevieveezeh786@gmail.com>
2023-04-27 19:24:02 +0000

    updated user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -12,6 +13,7 @@ class User(BaseModel, Base):
     """Representation of a user """
     if models.storage_t == 'db':
         __tablename__ = 'users'
+        __table_args__ = ({'mysql_default_charset': 'latin1'})
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -27,3 +29,12 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+        @property
+        def password(self):
+            return self._password
+
+        @password.setter
+        def password(self, pwd):
+            """hashing password values"""
+            self._password = hashlib.md5(pwd.encode()).hexdigest()
