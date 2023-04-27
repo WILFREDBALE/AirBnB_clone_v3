249d1c0fbe724744f7512c6e03fbcb5bdabf22f0
Goldjay94 <genevieveezeh786@gmail.com>
2023-04-27 19:17:56 +0000

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -68,6 +68,10 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if models.storage_t == "db":
+            # if "_password" in new_dict:
+            #     del new_dict["_password"]
+            new_dict.pop("_password", None)
         return new_dict
 
     def delete(self):
